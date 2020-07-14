fb8ceff6bef1090e757dd687301bf6c4fa9b69d6
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2020-07-14 12:29:34 +0700

    :hammer: Switch to vuex-class decorators for State mapping

--- components/specifics/Home/Example/script.ts
+++ components/specifics/Home/Example/script.ts
@@ -1,33 +1,21 @@
-import { Component, Vue, Mutation } from 'nuxt-property-decorator'
-import { mapState } from 'vuex'
-import { RootState } from '~/store/state'
+import { Component, Vue, Mutation, State } from 'nuxt-property-decorator'
 import { Auth } from '~/store/types/interfaces/auth.interface'
-@Component({
-  computed: {
-    ...mapState({
-      example: 'locale',
-      auth: 'auth'
-    })
-    // other stuff
-  }
-})
+@Component({})
 export default class Example extends Vue {
   randomNumber: number = 10
-  // Your can get the state by mapState, then declare it and its type like this:
-  example!: string
-  // Map auth using mapState above, then declare it inside this class
-  auth!: RootState['auth']
+  @State('example') example!: string
+  @State('auth') auth!: Auth
+  // Mutation mapping with type check for payload example:
+  @Mutation('SET_AUTH') SET_AUTH!: Auth
   // Then, if you want to get into auth and get currentUser
   get currentUser() {
     return this.auth.currentUser
   }
 
-  // Mutation mapping with type check for payload example:
-  @Mutation('SET_AUTH') SET_AUTH!: Auth
-
   mounted() {
     // this.randomNumber = this.currentUser // Type error
     // this.SET_AUTH(this.randomNumber) // Type error
     this.$emit('randomBus')
+    console.log(this.currentUser)
   }
 }
