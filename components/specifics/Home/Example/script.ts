78e6a517ba08e3080bb911fee3c0f76e164b6b51
Bittermeatball <nguyenhuunguyeny.ny@gmail.com>
2020-07-14 11:27:55 +0700

    :pencil: Add more example on VueX states mapping

--- components/specifics/Home/Example/script.ts
+++ components/specifics/Home/Example/script.ts
@@ -4,20 +4,26 @@ import { RootState } from '~/store/state'
 @Component({
   computed: {
     ...mapState({
-      example: 'locale'
+      example: 'locale',
+      auth: 'auth'
     })
     // other stuff
   }
 })
 export default class Example extends Vue {
-  // Your can get the state by mapState, then declare type like this:
+  // Your can get the state by mapState, then declare it and its type like this:
   example!: string
+  // Map auth using mapState above, then declare it inside this class
+  auth!: RootState['auth']
+  // Then
+  currentUser = this.auth.currentUser
   // Or you can do this by hand:
-  get currentUser() {
-    return (this.$store.state as RootState).auth.currentUser
-  }
+  // get currentUser() {
+  //   return (this.$store.state as RootState).auth.currentUser
+  // }
 
   mounted() {
     this.$emit('randomBus')
+    console.log(typeof this.example)
   }
 }
